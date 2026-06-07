8c8d0fcd9013e95fbc13e3c9f44385482db29280
Ahmadreza Ansari <ahmadreza.an.dev@gmail.com>
2026-06-07 18:40:58 +0330

    check

--- src/app/api/auth/[nextauth...]/route.ts
+++ src/app/api/auth/[nextauth...]/route.ts
@@ -1,19 +1,21 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+console.log("NEXTAUTH_SECRET =", process.env.NEXTAUTH_SECRET);
+
 const handler = NextAuth({
-  secret: process.env.NEXTAUTH_SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
 
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }),
+    ],
 
-  session: {
-    strategy: "jwt",
-  },
+    session: {
+        strategy: "jwt",
+    },
 });
 
 export { handler as GET, handler as POST };
