c0017f60ba55e2d363d92d065defeca0266b99fd
Ahmadreza Ansari <ahmadreza.an.dev@gmail.com>
2026-06-07 22:01:21 +0330

    change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { Providers } from "@/shared";
 import "./globals.css";
 import { Metadata } from "next";
 import RegisterSw from "@/components/RegisterSw";
-// import AuthProvider from "@/providers/AuthProviders";
+import AuthProvider from "@/providers/AuthProviders";
 
 export const metadata: Metadata = {
     title: {
@@ -16,12 +16,12 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     return (
         <html lang="en">
             <body className="p-0 m-0">
-                <Providers>
-                    {/* <AuthProvider> */}
-                        <RegisterSw/>
+                <AuthProvider>
+                    <Providers>
+                        <RegisterSw />
                         {children}
-                    {/* </AuthProvider> */}
-                </Providers>
+                    </Providers>
+                </AuthProvider>
             </body>
         </html >
     );
